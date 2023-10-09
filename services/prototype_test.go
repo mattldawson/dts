be020a76c0986040d06225452bc4353f5137ce60
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-10-09 09:47:44 -0700

    Putting together an end-to-end Globus transfer test.

--- services/prototype_test.go
+++ services/prototype_test.go
@@ -2,6 +2,7 @@ package services
 
 // unit test setup for the DTS prototype service
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/json"
@@ -10,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"slices"
 	"testing"
 	"time"
 
@@ -25,8 +27,10 @@ var CWD string
 // temporary testing directory
 var TESTING_DIR string
 
-// API prefix for requests.
-var API_PREFIX = "http://localhost:8080/api/v1/"
+var (
+	baseUrl   = "http://localhost:8080/"
+	apiPrefix = "api/v1/"
+)
 
 // service instance
 var service TransferService
@@ -45,8 +49,20 @@ databases:
     auth:
       client_id: ${JGI_CLIENT_ID}
       client_secret: ${JGI_CLIENT_SECRET}
+  kbase:
+    name: KBase
+    organization: KBase
+    url: https://kbase.us
+    endpoint: globus-kbase
 endpoints:
   globus-jdp:
+    name: Globus NERSC DTN
+    id: ${DTS_GLOBUS_TEST_ENDPOINT}
+    provider: globus
+    auth:
+      client_id: ${DTS_GLOBUS_CLIENT_ID}
+      client_secret: ${DTS_GLOBUS_CLIENT_SECRET}
+  globus-kbase:
     name: Globus NERSC DTN
     id: ${DTS_GLOBUS_TEST_ENDPOINT}
     provider: globus
@@ -126,11 +142,24 @@ func get(resource string) (*http.Response, error) {
 	}
 }
 
+// sends a POST query with well-formed headers and a payload
+func post(resource string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodPost, resource, body)
+	if err == nil {
+		accessToken := os.Getenv("DTS_KBASE_DEV_TOKEN")
+		b64Token := base64.StdEncoding.EncodeToString([]byte(accessToken))
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", b64Token))
+		return http.DefaultClient.Do(req)
+	} else {
+		return nil, err
+	}
+}
+
 // queries the service's root endpoint
 func TestQueryRoot(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
-	resp, err := get("http://localhost:8080/")
+	resp, err := get(baseUrl)
 	assert.Nil(err)
 
 	respBody, err := io.ReadAll(resp.Body)
@@ -146,9 +175,9 @@ func TestQueryRoot(t *testing.T) {
 
 // queries the service's databases endpoint
 func TestQueryDatabases(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
-	resp, err := get(API_PREFIX + "databases")
+	resp, err := get(baseUrl + apiPrefix + "databases")
 	assert.Nil(err)
 
 	respBody, err := io.ReadAll(resp.Body)
@@ -158,17 +187,29 @@ func TestQueryDatabases(t *testing.T) {
 	var dbs []dbMetadata
 	err = json.Unmarshal(respBody, &dbs)
 	assert.Nil(err)
-	assert.Equal(1, len(dbs))
+	assert.Equal(2, len(dbs))
+	slices.SortFunc(dbs, func(a, b dbMetadata) int {
+		if a.Id < b.Id {
+			return -1
+		} else if a.Id == b.Id {
+			return 0
+		} else {
+			return 1
+		}
+	})
 	assert.Equal("jdp", dbs[0].Id)
 	assert.Equal("JGI Data Portal", dbs[0].Name)
 	assert.Equal("Joint Genome Institute", dbs[0].Organization)
+	assert.Equal("kbase", dbs[1].Id)
+	assert.Equal("KBase", dbs[1].Name)
+	assert.Equal("KBase", dbs[1].Organization)
 }
 
 // queries a specific (valid) database
 func TestQueryValidDatabase(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
-	resp, err := get(API_PREFIX + "databases/jdp")
+	resp, err := get(baseUrl + apiPrefix + "databases/jdp")
 	assert.Nil(err)
 
 	respBody, err := io.ReadAll(resp.Body)
@@ -185,18 +226,18 @@ func TestQueryValidDatabase(t *testing.T) {
 
 // queries a database that doesn't exist
 func TestQueryInvalidDatabase(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
-	resp, err := get(API_PREFIX + "databases/nonexistentdb")
+	resp, err := get(baseUrl + apiPrefix + "databases/nonexistentdb")
 	assert.Nil(err)
 	assert.Equal(404, resp.StatusCode)
 }
 
 // searches a specific database for files matching a simple query
 func TestSearchDatabase(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
-	resp, err := get(API_PREFIX + "files?database=jdp&query=prochlorococcus")
+	resp, err := get(baseUrl + apiPrefix + "files?database=jdp&query=prochlorococcus")
 	assert.Nil(err)
 
 	respBody, err := io.ReadAll(resp.Body)
@@ -211,17 +252,54 @@ func TestSearchDatabase(t *testing.T) {
 	assert.NotNil(results.Resources)
 }
 
+// creates a transfer for a single file
+func TestCreateTransfer(t *testing.T) {
+	assert := assert.New(t)
+
+	// The JDP database currently relies on JAMO to locate files with given
+	// IDs, so we can only run this test when the DTS is running within LBL's
+	// virtual private network.
+	if _, onVPN := os.LookupEnv("DTS_ON_LBL_VPN"); onVPN { // any value will work
+		// request a transfer of Ga0451497_cog.gff
+		payload, err := json.Marshal(TransferRequest{
+			Source:      "jdp",
+			FileIds:     []string{"JDP:5fa4fb4547675a20c852c5f8"},
+			Destination: "kbase",
+		})
+		resp, err := post(baseUrl+apiPrefix+"transfers", bytes.NewReader(payload))
+		assert.Nil(err)
+		assert.Equal(200, resp.StatusCode)
+		if err == nil {
+			defer resp.Body.Close()
+			var body []byte
+			body, err = io.ReadAll(resp.Body)
+			assert.Nil(err)
+			var xferResp TransferResponse
+			err = json.Unmarshal(body, &xferResp)
+			xferId := xferResp.Id
+
+			// get the transfer status
+			resp, err := get(baseUrl + apiPrefix + fmt.Sprintf("transfers/%s", xferId.String()))
+			assert.Nil(err)
+			assert.Equal(200, resp.StatusCode)
+		}
+	} else {
+		log.Printf("Skipping (JAMO-backed) JDP transfer tests")
+		log.Printf("To enable these tests, set the DTS_ON_LBL_VPN environment variable")
+	}
+}
+
 // attempts to fetch the status of a nonexistent transfer
 func TestFetchInvalidTransferStatus(t *testing.T) {
-	assert := assert.New(t) // binds assert to t
+	assert := assert.New(t)
 
 	// try an ill-formed transfer
-	resp, err := get(API_PREFIX + "transfers/xyzzy")
+	resp, err := get(baseUrl + apiPrefix + "transfers/xyzzy")
 	assert.Nil(err)
 	assert.Equal(400, resp.StatusCode)
 
 	// I bet this one doesn't exist!!
-	resp, err = get(API_PREFIX + "transfers/3f0f9563-e1f8-4b9c-9308-36988e25df0b")
+	resp, err = get(baseUrl + apiPrefix + "transfers/3f0f9563-e1f8-4b9c-9308-36988e25df0b")
 	assert.Nil(err)
 	assert.Equal(404, resp.StatusCode)
 }

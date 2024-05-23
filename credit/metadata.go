d2631aebf802889ba52ca7924df95af8e6398d09
jeff-cohere <7560777+jeff-cohere@users.noreply.github.com>
2024-05-23 21:53:12 +0000

    [create-pull-request] automated change

--- credit/metadata.go
+++ credit/metadata.go
@@ -1,29 +1,75 @@
 package credit
 
 /*
- * Container for an instance of credit metadata; includes the credit metadata itself and metadata for the credit metadata.
+ * Represents a contributor to the resource.
+
+Contributors must have a 'contributor_type', either 'Person' or 'Organization', and
+one of the 'name' fields: either 'given_name' and 'family_name' (for a person), or 'name' (for an organization or a person).
+
+The 'contributor_role' field takes values from the DataCite and CRediT contributor
+roles vocabularies. For more information on these resources and choosing
+appropriate roles, please see the following links:
+
+DataCite contributor roles: https://support.datacite.org/docs/datacite-metadata-schema-v44-recommended-and-optional-properties#7a-contributortype
+
+CRediT contributor role taxonomy: https://credit.niso.org
+
+ */
+type Contributor struct {
+	/*
+	 * Must be either 'Person' or 'Organization'
+	 */
+	ContributorType string `json:"contributor_type"`
+	/*
+	 * Persistent unique identifier for the contributor; this might be an ORCID for an individual, or a ROR ID for an organization.
+	 */
+	ContributorId string `json:"contributor_id"`
+	/*
+	 * Contributor name. For organizations, this should be the full (unabbreviated) name; can also be used for a person if the given name/family name format is not applicable.
+	 */
+	Name string `json:"name"`
+	/*
+	 * The given name(s) of the contributor.
+	 */
+	GivenName string `json:"given_name"`
+	/*
+	 * The family name(s) of the contributor.
+	 */
+	FamilyName string `json:"family_name"`
+	/*
+	 * List of organizations with which the contributor is affiliated. For contributors that represent an organization, this may be a parent organization (e.g. KBase, US DOE; Arkin lab, LBNL).
+	 */
+	Affiliations []Organization `json:"affiliations"`
+	/*
+	 * List of roles played by the contributor when working on the resource.
+	 */
+	ContributorRoles string `json:"contributor_roles"`
+}
+
+/*
+ * Container for an instance of credit metadata; includes the credit metadata itself and metadata for the credit metadata, including the schema version used, who submitted it, and the date of submission. When the credit metadata for a resource is added or updated, a new CreditMetadataEntry is created to store the information.
  */
 type CreditMetadataEntry struct {
 	/*
-	 * the credit metadata itself
+	 * The credit metadata itself.
 	 */
 	CreditMetadata CreditMetadata `json:"credit_metadata"`
 	/*
-	 * the version of the credit metadata schema used
+	 * The version of the credit metadata schema used.
 	 */
 	CreditMetadataSchemaVersion string `json:"credit_metadata_schema_version"`
 	/*
-	 * KBase workspace ID of the user who added this entry
+	 * KBase workspace ID of the user who added this entry.
 	 */
 	SavedBy string `json:"saved_by"`
 	/*
-	 * unix timestamp for the addition of this set of credit metadata
+	 * Unix timestamp for the addition of this set of credit metadata.
 	 */
 	Timestamp int `json:"timestamp"`
 }
 
 /*
-  - Represents the credit metadata associated with a workspace object.
+ * Represents the credit metadata associated with a workspace object.
 
 In the following documentation, 'Resource' is used to refer to the workspace object
 that the CM pertains to.
@@ -37,11 +83,11 @@ Currently the KBase workspace only supports credit metadata for objects of type
 'dataset'; anything else will return an error.
 
 The license may be supplied either as an URL pointing to licensing information for
-the resource, or using a license name. See https://choosealicense.com/appendix/ for
-a list of common open source licenses.
+the resource, or using an SPDX license identifier from the list maintained at https://spdx.org/licenses/.
 
 Required fields are:
 - identifier
+- resource_type
 - versioning information: if the resource does not have an explicit version number,
 one or more dates should be supplied: ideally the date of resource publication and
 the last update (if applicable).
@@ -50,151 +96,129 @@ the last update (if applicable).
 
 The resource_type field is required, but as there is currently only a single valid
 value, 'dataset', it is automatically populated if no value is supplied.
-*/
+
+ */
 type CreditMetadata struct {
 	/*
-	 * list of strings of freeform text providing extra information about this credit metadata.
+	 * List of strings of freeform text providing extra information about this credit metadata.
 	 */
 	Comment string `json:"comment"`
 	/*
-	 * A brief description or abstract for the resource being represented.
+	 * The URL of the content of the resource.
 	 */
-	Description string `json:"description"`
+	ContentUrl string `json:"content_url"`
 	/*
-	 * resolvable persistent unique identifier for the resource. Should be in the format <database name>:<identifier within database>.
-	 */
-	Identifier string `json:"identifier"`
-	/*
-			 * usage license for the resource. May be a text string or an URL. Abbreviations should be spelled out where possible (e.g. 'Creative Commons 4.0' instead of 'CC-BY-4.0'). The license is interpreted as an URL and checked for well-formedness if it starts with a series of letters, a colon, and slashes, e.g. "http://"; "https://"; "ftp://".
-
-		All data published at KBase is done so under a Creative Commons 0 or Creative Commons 4.0 license.
-
-	*/
-	License string `json:"license"`
-	/*
-	 * the broad type of the source data for this workspace object. 'dataset' is the only valid value currently.
-	 */
-	ResourceType string `json:"resource_type"`
-	/*
-	 * the version of the resource. This must be an absolute version, not a relative version like 'latest'.
-	 */
-	Version string `json:"version"`
-	/*
-	 * a list of people and/or organizations who contributed to the resource.
+	 * A list of people and/or organizations who contributed to the resource.
 	 */
 	Contributors []Contributor `json:"contributors"`
 	/*
-	 * a list of relevant lifecycle events for the resource.
+	 * A list of CURIEs, URIs, or free text entries denoting the source of the credit metadata.
+	 */
+	CreditMetadataSource string `json:"credit_metadata_source"`
+	/*
+	 * A list of relevant lifecycle events for the resource. Note that these dates apply only to the resource itself, and not to the creation or update of the credit metadata record for the resource.
 	 */
 	Dates []EventDate `json:"dates"`
 	/*
-	 * funding sources for the resource.
+	 * A brief description or abstract for the resource being represented.
+	 */
+	Descriptions []Description `json:"descriptions"`
+	/*
+	 * Funding sources for the resource.
 	 */
 	Funding []FundingReference `json:"funding"`
 	/*
-	 * other resolvable persistent unique IDs related to the resource.
+	 * Resolvable persistent unique identifier for the resource. Should be in the format <database name>:<identifier within database>.
+	 */
+	Identifier string `json:"identifier"`
+	/*
+	 * Usage license for the resource. Use one of the SPDX license identifiers or provide a link to the license text if no SPDX ID is available.
+
+All data published at KBase is done so under a Creative Commons 0 or Creative Commons 4.0 license.
+
+	 */
+	License License `json:"license"`
+	/*
+	 * The publisher of the resource. For a dataset, this is the repository where it is stored.
+	 */
+	Publisher Organization `json:"publisher"`
+	/*
+	 * Other resolvable persistent unique IDs related to the resource.
 	 */
 	RelatedIdentifiers []PermanentID `json:"related_identifiers"`
 	/*
-	 * online repository for a dataset.
+	 * The broad type of the source data for this workspace object. 'dataset' is currently the only valid value for KBase DOIs.
 	 */
-	Repository Organization `json:"repository"`
+	ResourceType string `json:"resource_type"`
 	/*
-	 * one or more titles for the resource. At least one title of title_type "title" must be provided.
+	 * One or more titles for the resource.
 	 */
 	Titles []Title `json:"titles"`
+	/*
+	 * The URL of the resource.
+	 */
+	Url string `json:"url"`
+	/*
+	 * The version of the resource. This must be an absolute version, not a relative version like 'latest'.
+	 */
+	Version string `json:"version"`
 }
 
 /*
-  - Represents a contributor to the resource.
-
-Contributors must have a 'contributor_type', either 'Person' or 'Organization', and
-a 'name'.
-
-The 'credit_name' field is used to store the name of a person as it would appear in
-a citation. If there is no 'credit_name' supplied, the 'name' field would be used
-in citations.
-For example:
-
-	name:         Hubert George Wells
-	credit_name:  Wells, HG
-
-	name:         Alexandria Ocasio-Cortez
-	credit_name:  Ocasio-Cortez, A
-
-	name:         Helena Bonham Carter
-	credit_name:  Bonham Carter, H
-
-The 'contributor_role' field takes values from the DataCite and CRediT contributor
-roles vocabularies. For more information on these resources and choosing the
-appropriate roles, please see the following links:
-
-DataCite contributor roles: https://support.datacite.org/docs/datacite-metadata-schema-v44-recommended-and-optional-properties#7a-contributortype
-
-CRediT contributor role taxonomy: https://credit.niso.org
-*/
-type Contributor struct {
+ * Textual information about the resource being represented.
+ */
+type Description struct {
 	/*
-	 * must be either 'Person' or 'Organization'
+	 * The text content of the informational element.
 	 */
-	ContributorType string `json:"contributor_type"`
+	DescriptionText string `json:"description_text"`
 	/*
-	 * persistent unique identifier for the contributor; this might be an ORCID for an individual, or a ROR ID for an organization.
+	 * The type of text being represented
 	 */
-	ContributorId string `json:"contributor_id"`
+	DescriptionType string `json:"description_type"`
 	/*
-	 * contributor name. For organizations, this should be the full (unabbreviated) name; for a person, the full name should be entered.
+	 * The language in which the description is written, using the appropriate IETF BCP-47 notation.
 	 */
-	Name string `json:"name"`
-	/*
-	 * for a person, how the name should appear in a citation.
-	 */
-	CreditName string `json:"credit_name"`
-	/*
-	 * list of organizations with which the contributor is affiliated. For contributors that represent an organization, this may be a parent organization (e.g. KBase, US DOE; Arkin lab, LBNL).
-	 */
-	Affiliations []Organization `json:"affiliations"`
-	/*
-	 * list of roles played by the contributor when working on the resource.
-	 */
-	ContributorRoles string `json:"contributor_roles"`
+	Language string `json:"language"`
 }
 
 /*
-  - Represents an event in the lifecycle of a resource and the date it occurred on.
+ * Represents an event in the lifecycle of a resource and the date it occurred on.
 
 See https://support.datacite.org/docs/datacite-metadata-schema-v44-recommended-and-optional-properties#8-date for more information on the events.
-*/
+
+ */
 type EventDate struct {
 	/*
-	 * the date associated with the event. The date may be in the format YYYY, YYYY-MM, or YYYY-MM-DD.
+	 * The date associated with the event. The date may be in the format YYYY, YYYY-MM, or YYYY-MM-DD.
 	 */
 	Date string `json:"date"`
 	/*
-	 * the nature of the resource-related event that occurred on that date
+	 * The nature of the resource-related event that occurred on that date.
 	 */
 	Event string `json:"event"`
 }
 
 /*
-  - Represents a funding source for a resource, including the funding body and the grant awarded.
+ * Represents a funding source for a resource, including the funding body and the grant awarded.
 
 The 'funder_name' field is required; all others are optional.
 
 Recommended resources for organization identifiers include:
   - Research Organization Registry, http://ror.org
   - International Standard Name Identifier, https://isni.org
-  - Crossref Funder Registry, https://www.crossref.org/services/funder-registry/
+  - Crossref Funder Registry, https://www.crossref.org/services/funder-registry/ (to be subsumed into ROR)
 
 Some organizations may have a digital object identifier (DOI).
-*/
+
+ */
 type FundingReference struct {
 	/*
-	 * code for the grant, assigned by the funder
+	 * Code for the grant, assigned by the funder
 	 */
 	GrantId string `json:"grant_id"`
 	/*
-	 * title for the grant
+	 * Title for the grant
 	 */
 	GrantTitle string `json:"grant_title"`
 	/*
@@ -202,13 +226,27 @@ type FundingReference struct {
 	 */
 	GrantUrl string `json:"grant_url"`
 	/*
-	 * the funder for the grant or award
+	 * The funder for the grant or award
 	 */
 	Funder Organization `json:"funder"`
 }
 
 /*
-  - Represents an organization.
+ * License information for the resource.
+ */
+type License struct {
+	/*
+	 * String representing the license, from the SPDX license identifiers at https://spdx.org/licenses/.
+	 */
+	Id string `json:"id"`
+	/*
+	 * URL for the license.
+	 */
+	Url string `json:"url"`
+}
+
+/*
+ * Represents an organization.
 
 Recommended resources for organization identifiers and canonical organization names include:
   - Research Organization Registry, http://ror.org
@@ -216,73 +254,69 @@ Recommended resources for organization identifiers and canonical organization na
   - Crossref Funder Registry, https://www.crossref.org/services/funder-registry/
 
 For example, the US DOE would be entered as:
+  organization_name: United States Department of Energy
+  organization_id:   ROR:01bj3aw27
 
-	organization_name: United States Department of Energy
-	organization_id:   ROR:01bj3aw27
-*/
+ */
 type Organization struct {
 	/*
-	 * persistent unique identifier for the organization in the format <database name>:<identifier within database>
+	 * Persistent unique identifier for the organization in the format <database name>:<identifier within database>
 	 */
 	OrganizationId string `json:"organization_id"`
 	/*
-	 * common name of the organization; use the name recommended by ROR if possible.
+	 * Common name of the organization; use the name recommended by ROR if possible.
 	 */
 	OrganizationName string `json:"organization_name"`
 }
 
 /*
-  - Represents a persistent unique identifier for an entity, with an optional relationship to some other entity.
+ * Represents a persistent unique identifier for an entity, with an optional relationship to some other entity.
 
-The 'id' field is required; all other fields are optional.
+The 'id' field and 'relationship_type' fields are required.
 
 The values in the 'relationship_type' field come from controlled vocabularies maintained by DataCite and Crossref. See the documentation links below for more details.
 
 DataCite relation types: https://support.datacite.org/docs/datacite-metadata-schema-v44-recommended-and-optional-properties#12b-relationtype
 
 Crossref relation types: https://www.crossref.org/documentation/schema-library/markup-guide-metadata-segments/relationships/
-*/
+
+ */
 type PermanentID struct {
 	/*
-	 * persistent unique ID for an entity. Should be in the format <database name>:<identifier within database>.
+	 * Persistent unique ID for an entity. Should be in the format <database name>:<identifier within database>.
 	 */
 	Id string `json:"id"`
 	/*
-	 * description of that entity
+	 * Description of that entity.
 	 */
 	Description string `json:"description"`
 	/*
-			 * The relationship between the ID and some other entity.
-		For example, when a PermanentID class is used to represent objects in the CreditMetadata field 'related_identifiers', the 'relationship_type' field captures the relationship between the CreditMetadata and this ID.
+	 * The relationship between the ID and some other entity.
+For example, when a PermanentID class is used to represent objects in the CreditMetadata field 'related_identifiers', the 'relationship_type' field captures the relationship between the CreditMetadata and this ID.
 
-	*/
+	 */
 	RelationshipType string `json:"relationship_type"`
 }
 
 /*
-  - Represents the title or name of a resource.
+ * Represents the title or name of a resource, the type of that title, and the language used (if appropriate).
 
-The 'title_string' field is required; if no value is supplied for 'title_type', it
-defaults to 'title'.
+The 'title' field is required; 'title_type' is only necessary if the text is not the primary title.
 
-If the title is in a language other than English, the 'title_type' should be set to
-'translated_title', and the appropriate BCP-47 tag supplied in the 'title_language'
-field.
-
-Note that the workspace checks that the 'title_language' field adheres to IETF
-BCP-47 syntax rules, but it does not check the validity of the tag.
-*/
+ */
 type Title struct {
 	/*
-	 * a string used as a title for a resource
+	 * The language in which the title is written, using the appropriate IETF BCP-47 notation.
+	 */
+	Language string `json:"language"`
+	/*
+	 * A string used as a title for a resource
 	 */
 	Title string `json:"title"`
 	/*
-	 * a descriptor for the title. The default value is 'title'.
+	 * A descriptor for the title for cases where the contents of the 'title' field is not the primary name or title.
 	 */
 	TitleType string `json:"title_type"`
-	/*
-	 * language that the title is in, as a IETF BCP-47 tag.
-	 */
-	TitleLanguage string `json:"title_language"`
 }
+
+

44b27642006d9375633624a0228c60642fb8891d
Jeffrey N. Johnson <jeff@cohere-llc.com>
2025-03-20 10:35:31 -0700

    Some minor modifications to allow Docker deployment with updated modules.

--- services/prototype.go
+++ services/prototype.go
@@ -142,7 +142,7 @@ func (service *prototype) Close() {
 // Version numbers
 var majorVersion = 0
 var minorVersion = 4
-var patchVersion = 0
+var patchVersion = 1
 
 // Version string
 var version = fmt.Sprintf("%d.%d.%d", majorVersion, minorVersion, patchVersion)

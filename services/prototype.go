ec6c9fb33ed09313de8982318d6d45b2c7430068
Jeffrey N. Johnson <jeff@cohere-llc.com>
2025-07-09 14:02:52 -0700

    Bumped minor version number.

--- services/prototype.go
+++ services/prototype.go
@@ -132,8 +132,8 @@ func (service *prototype) Close() {
 
 // Version numbers
 var majorVersion = 0
-var minorVersion = 7
-var patchVersion = 1
+var minorVersion = 8
+var patchVersion = 0
 
 // Version string
 var version = fmt.Sprintf("%d.%d.%d", majorVersion, minorVersion, patchVersion)
